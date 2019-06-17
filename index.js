88ec25d83e01db5f0bd30182f18d67fc1a5ca57a
Technotype <gamingmasterdonttryit78@gmail.com>
2019-06-17 12:18:46 +0200

    Started object development and added JSON file type
    Also, I'm changing it to badwords.js

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-module.exports = require('./array.js');
-// If no list type is specified, it will default to array
+let arr = require('./array')
+
+class Main {
+  setOptions(options = {
+    censor: '*',
+    blacklist: arr,
+    whitelist: []
+  }) {
+    this.options = options;
+    return this;
+  }
+  clear(str) {
+    let r = str;
+    for (let i = 0; i < arr.length; i++) {
+      let s = "";
+      for (let j = 0; j <= arr[i].length; i++) {
+        s += "*";
+      }
+      r.replace(arr[i], s);
+    }
+    return r;
+  }
+}
+
+module.exports.ARRAY = require('./array');
+module.exports.OBJECT = require('./object');
+module.exports.JSON = require('./json');
+
+module.exports = Main;
