3fa980e0f40f0e825dcfbaa8376577a4f2db164b
TechnotypeO <gamingmasterdonttryit78@gmail.com>
2020-04-28 16:20:28 +0200

    Fix index.js
    Well, of course I had done something bad that I don't like so I fixed it. Oh, and I fixed some type errors. Yeah... that one infuriated me when I made this package. You can't blame me, I was stupid.

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
 let arr = require('./array')
 let pkg = require('./package')
+
 class Main {
-  getInfo() {
-    return {
-      name: pkg.name,
-      version: pkg.version,
-      description: pkg.description,
-      updates: pkg.updates
+    getInfo() {
+        return {
+            name: pkg.name,
+            version: pkg.version,
+            description: pkg.description,
+            updates: pkg.updates
+        }
     }
-  }
-  setOptions(options = {
-    censor: '*',
-    blacklist: arr,
-    whitelist: []
-  }) {
-    this.options = options;
-    arr = arr.filter((item, index, arr) => {
-      return !whitelist.includes(item);
-    })
-    return this;
-  }
-  /**
-   * Returns whether or not a string contains profane
-   * language
-   *
-   * @param {string} string
-   * @returns {Boolean} whether or not it is profane
-   */
-  isProfane(str) {
-    let string = str.trim().split(/ +/g);
-    for (let i = 0; i < string.length; i++) {
-      if (arr.includes(string[i])) return true;
+    setOptions(options = {
+        censor: '*',
+        blacklist: arr,
+        whitelist: []
+    }) {
+        this.options = options;
+        arr = arr.filter((item, index, arr) => {
+            return !whitelist.includes(item);
+        })
+        return this;
     }
-    return false;
-  }
-  /**
-   * Clears a string off profane language
-   *
-   * @param {string} string
-   * @returns {Boolean}
-   */
-  clear(str) {
-    let r = str;
-    for (let i = 0; i < arr.length; i++) {
-      let s = "";
-      for (let j = 0; j <= arr[i].length; i++) {
-        s += "*";
-      }
-      r.replace(arr[i], s);
+    /**
+     * Returns whether or not a string contains profane
+     * language
+     *
+     * @param {string} str
+     * @returns {Boolean} whether or not it is profane
+     */
+    isProfane(str) {
+        let string = str.trim().split(/ +/g);
+        for (let i = 0; i < string.length; i++) {
+            if (arr.includes(string[i])) return true;
+        }
+        return false;
     }
-    return r;
-  }
+    /**
+     * Clears a string off profane language
+     *
+     * @param {string} str
+     * @returns {Boolean}
+     */
+    clear(str) {
+        let r = str;
+        for (let i = 0; i < arr.length; i++) {
+            let s = "";
+            for (let j = 0; j <= arr[i].length; i++) {
+                s += "*";
+            }
+            r.replace(arr[i], s);
+        }
+        return r;
+    }
 
 }
 
-exports.ARRAY = require('./array.js');
-exports.OBJECT = require('./object.js');
-exports.JSON = require('./json.json');
-module.exports.REGEXP = require('./regexp.js');
+export const ARRAY = require('./array.js');
+export const OBJECT = require('./object.js');
+export const JSON = require('./json.json');
+export const REGEXP = require('./regexp.js');
 
 module.exports = Main;
